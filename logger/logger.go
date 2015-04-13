75ab9cf4f7e0db12d0ca1e7fe29e1cb0deec7c65
Tim Lamballais Tessensohn <tim@basenet.nl>
2015-04-13 16:31:50 +0200

    Make all log methods do formatting.

--- logger/logger.go
+++ logger/logger.go
@@ -24,22 +24,22 @@ func (l Log) Error(e error) {
 	log.Fatal(e)
 }
 
-func (l Log) Warn(msg string) {
-	l.log(msg, Loglevel_Warn)
+func (l Log) Warn(msg string, args ...interface{}) {
+	l.log(msg, Loglevel_Warn, args...)
 }
 
-func (l Log) Info(msg string) {
-	l.log(msg, Loglevel_Info)
+func (l Log) Info(msg string, args ...interface{}) {
+	l.log(msg, Loglevel_Info, args...)
 }
 
 func (l Log) Debug(msg string, args ...interface{}) {
+	l.log(msg, Loglevel_Debug, args...)
+}
+
+func (l Log) log(msg string, level int, args ...interface{}) {
 	if len(args) > 0 {
 		msg = fmt.Sprintf(msg, args...)
 	}
-	l.log(msg, Loglevel_Debug)
-}
-
-func (l Log) log(msg string, level int) {
 	if (l.logLevel >= level) {
 		log.Println(msg)
 	}
