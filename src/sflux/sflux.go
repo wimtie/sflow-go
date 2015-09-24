4d6b1e249c1ca564faa0a76bf70c6ce68fe6b12b
Willem Fibbe <fibbers@gmail.com>
2015-09-24 17:52:17 +0200

    Format source-code and use updated influxdb-API
    - Source-code is formatted by vim-go.
    
    - github.com/influxdb/influxdb/ has an updated client.Point API, so modify
      accordingly

--- src/sflux/sflux.go
+++ src/sflux/sflux.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
-	"os"
 	"bufio"
-	"time"
 	"flag"
-	"strings"
-	"strconv"
-	"net/url"
+	"fmt"
 	"github.com/influxdb/influxdb/client"
+	"net/url"
+	"os"
+	"reflect"
 	"sflux/logger"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type Counters struct {
-	Source string
-	IfIndex int64
-	IfType int64
-	IfSpeed int64
-	IfDirection int64 // Derived from MAU MIB (RFC 2668) 0 = unknown, 1 = full-duplex, 2 = half-duplex, 3 = in, 4 = out
-	IfStatus int64
-	IfInOctets int64
-	IfInUcastPkts int64
-	IfInMulticastPkts int64
-	IfInBroadcastPkts int64
-	IfInDiscards int64
-	IfInErrors int64
-	IfInUnknownProtos int64
-	IfOutOctets int64
-	IfOutUcastPkts int64
+	Source             string
+	IfIndex            int64
+	IfType             int64
+	IfSpeed            int64
+	IfDirection        int64 // Derived from MAU MIB (RFC 2668) 0 = unknown, 1 = full-duplex, 2 = half-duplex, 3 = in, 4 = out
+	IfStatus           int64
+	IfInOctets         int64
+	IfInUcastPkts      int64
+	IfInMulticastPkts  int64
+	IfInBroadcastPkts  int64
+	IfInDiscards       int64
+	IfInErrors         int64
+	IfInUnknownProtos  int64
+	IfOutOctets        int64
+	IfOutUcastPkts     int64
 	IfOutMulticastPkts int64
 	IfOutBroadcastPkts int64
-	IfOutDiscards int64
-	IfOutErrors int64
-	IfPromiscuousMode int64
-	TimeStamp time.Time
+	IfOutDiscards      int64
+	IfOutErrors        int64
+	IfPromiscuousMode  int64
+	TimeStamp          time.Time
 }
 
 var MetricFields = [...]string{"IfInOctets", "IfOutOctets", "IfInDiscards", "IfInBroadcastPkts", "IfInMulticastPkts", "IfInUcastPkts", "IfOutUcastPkts", "IfOutMulticastPkts", "IfOutBroadcastPkts", "IfOutDiscards", "IfOutErrors"}
@@ -76,7 +76,7 @@ func main() {
 
 func run() {
 	scanner := bufio.NewScanner(os.Stdin)
-	all := scanN(scanner, chunkSize);
+	all := scanN(scanner, chunkSize)
 	for all != nil {
 		insertIntoInflux(all)
 		all = scanN(scanner, chunkSize)
@@ -95,30 +95,30 @@ func scanN(scanner *bufio.Scanner, n int) []Counters {
 		source := columns[1]
 		columns = columns[2:]
 		tmp := Counters{
-					source,
-					getInt(columns, 0),
-					getInt(columns, 1),
-					getInt(columns, 2),
-					getInt(columns, 3),
-					getInt(columns, 4),
-					getInt(columns, 5),
-					getInt(columns, 6),
-					getInt(columns, 7),
-					getInt(columns, 8),
-					getInt(columns, 9),
-					getInt(columns, 10),
-					getInt(columns, 11),
-					getInt(columns, 12),
-					getInt(columns, 13),
-					getInt(columns, 14),
-					getInt(columns, 15),
-					getInt(columns, 16),
-					getInt(columns, 17),
-					getInt(columns, 18),
-					timestamp,
+			source,
+			getInt(columns, 0),
+			getInt(columns, 1),
+			getInt(columns, 2),
+			getInt(columns, 3),
+			getInt(columns, 4),
+			getInt(columns, 5),
+			getInt(columns, 6),
+			getInt(columns, 7),
+			getInt(columns, 8),
+			getInt(columns, 9),
+			getInt(columns, 10),
+			getInt(columns, 11),
+			getInt(columns, 12),
+			getInt(columns, 13),
+			getInt(columns, 14),
+			getInt(columns, 15),
+			getInt(columns, 16),
+			getInt(columns, 17),
+			getInt(columns, 18),
+			timestamp,
 		}
 		all = append(all, tmp)
-		if (len(all) == n) {
+		if len(all) == n {
 			return all
 		}
 		count = count + 1
@@ -126,22 +126,24 @@ func scanN(scanner *bufio.Scanner, n int) []Counters {
 	return all
 }
 
-func appendPoints(points []client.Point, inst Counters) {
+func appendPoints(points []client.Point, counters []Counters, i int) {
+	inst := counters[i]
 	instVal := reflect.ValueOf(inst)
+	log.Debug("process line: %d", i)
 	for ii := range MetricFields {
 		fieldName := MetricFields[ii]
 		cVal := reflect.Indirect(instVal).FieldByName(fieldName).Int()
-		log.Debug("CREATE POINT #%d, counter: %s source:%s ifindex:%d: value:%d", i * len(MetricFields) + ii, fieldName, inst.Source, inst.IfIndex, cVal)
-		points[i * len(MetricFields) + ii] = client.Point {
-			Name: fieldName,
-			Tags: map[string]string {
-				"Source": inst.Source,
-				"IfIndex" : fmt.Sprintf("%d", inst.IfIndex),
+		log.Debug("CREATE POINT #%d, counter: %s source:%s ifindex:%d: value:%d", i*len(MetricFields)+ii, fieldName, inst.Source, inst.IfIndex, cVal)
+		points[i*len(MetricFields)+ii] = client.Point{
+			Measurement: fieldName,
+			Tags: map[string]string{
+				"Source":  inst.Source,
+				"IfIndex": fmt.Sprintf("%d", inst.IfIndex),
 			},
-			Fields: map[string]interface{} {
+			Fields: map[string]interface{}{
 				"value": cVal,
 			},
-			Timestamp: inst.TimeStamp,
+			Time:      inst.TimeStamp,
 			Precision: "s",
 		}
 	}
@@ -150,15 +152,14 @@ func appendPoints(points []client.Point, inst Counters) {
 func insertIntoInflux(counters []Counters) {
 	chunk := len(counters)
 	log.Debug("allocate Point slice of size %d", (chunk * len(MetricFields)))
-	pts := make([]client.Point, chunk * len(MetricFields))
+	pts := make([]client.Point, chunk*len(MetricFields))
+	//var pts []client.Point
 	for i := 0; i < chunk; i++ {
-		log.Debug("process line: %d", i)
-		inst := counters[i]
-		appendPoints(pts, inst)
+		appendPoints(pts, counters, i)
 	}
-	bps := client.BatchPoints {
-		Points: pts,
-		Database: influxDB,
+	bps := client.BatchPoints{
+		Points:          pts,
+		Database:        influxDB,
 		RetentionPolicy: "default",
 	}
 	con := getInfluxClient()
